7a7fa990403b9712489131e6a5b8e67885e40102
SuperCoder1999 <331150871@qq.com>
2021-04-22 23:24:10 +0800

    1

--- JavaSE/2021.4.21/src/Hello.java
+++ JavaSE/2021.4.21/src/Hello.java
@@ -1,5 +1,24 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello,idea");
+        T t = new T();
+    }
+}
+
+/**
+ * 构造器调用其他构造器，最多只能调用一个。
+ * 不能直接用其方法名调用。只能通过 this
+ */
+class T{
+    public T(){
+        this("nie",20);
+        System.out.println("无参构造器");
+    }
+    public T(String name,int age){
+        this(200);
+        System.out.println("无参构造器调用其他构造器");
+    }
+    public T(int money){
+        System.out.println("100w");
     }
 }
---
+++ JavaSE/2021.4.22/src/TheStarts.java
@@ -0,0 +1,11 @@
+public class TheStarts {
+    public static void main(String[] args) {
+        System.out.println("TheStarts");
+        
+    }
+}
+
+
+class Mytools {
+    int a;
+}
